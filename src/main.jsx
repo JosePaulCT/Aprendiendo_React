9d11ff98850fa0c63fac8462301a02c9165b5833
JosePaulCT <josepaulcamposterrones@gmail.com>
2023-12-19 21:23:24 -0500

    Tarjeta de seguir de X

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { App } from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+/*Primera nota:
+Los componentes deben ser PascalKeys, es porque no es capaz de
+diferenciar entre el elemento html a un componente*/
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+
+root.render(
+    <App/>
 )
