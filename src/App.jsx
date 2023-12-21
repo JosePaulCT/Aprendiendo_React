7c82f1f6ca60b433aedf813cd856cebcaab1df2a
JosePaulCT <josepaulcamposterrones@gmail.com>
2023-12-21 17:22:47 -0500

    Como hacer un boton que cambia el texto al estar el mouse encima MVP: hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 import { Follow_Card_X } from './Follow_Card_X';
+const  sebastian = {userCode : '@Sebastian', avatar: 'https://avatars.githubusercontent.com/u/75256717?v=4', follow: true};
+const  pedro = {userCode : '@Pedro', avatar: 'https://avatars.githubusercontent.com/u/75256717?v=4', follow: false};
 
 export function App() {
     return (
         <>
+        <Follow_Card_X {...sebastian}>
+        Sebatian Barrios
+        </Follow_Card_X>
+
+        <br/>
+
+        <Follow_Card_X {...pedro}>
+        Pedro Leopard Onepiece
+        </Follow_Card_X>
+
+        <br/>
+
         <Follow_Card_X
             userCode='@adriave'
             avatar='https://avatars.githubusercontent.com/u/75256717?v=4'
-            follow={true}
-        >
+            follow={true}>
+
         Adrian Castro Villanueva
         </Follow_Card_X>
 
         <br/>
+
         <Follow_Card_X
             userCode='@Pauldev'
             avatar='https://avatars.githubusercontent.com/u/75256717?v=4'
--- src/Follow_Card_X.jsx
+++ src/Follow_Card_X.jsx
@@ -1,6 +1,8 @@
+import PropTypes from 'prop-types';
 import './App.css'
 
 export function Follow_Card_X({children, userCode, avatar, follow}) {
+    const btn_Follow = follow ? 'ad-X-follow-card-btnFollow' : 'ad-X-follow-card';
     return (
         <article className='ad-X-follow-card'>
             <header className='ad-X-follow-card-header'>
@@ -10,11 +12,19 @@ export function Follow_Card_X({children, userCode, avatar, follow}) {
                     <span className='ad-X-follow-card-userCode'>{userCode}</span>
                 </div>
             </header>
-            <aside>
-                <button className='ad-X-follow-card-btnFollow'>
-                    {follow ? 'Siguiendo' : 'Seguir'}
-                </button>
+            <aside >
+            <button className={btn_Follow}>
+                <span className='initial'>Seguir</span>
+                <span className='hover'>✖ Dejar de seguir</span>
+            </button>
             </aside>
         </article>
     );
 }
+
+Follow_Card_X.propTypes = {
+    children: PropTypes.node.isRequired,
+    userCode: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    follow: PropTypes.bool.isRequired,
+};
